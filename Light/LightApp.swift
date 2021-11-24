3759fdc582f1e11edea1043a4af6748660491c2a
Joel Reyes <jreyes@centralstatesser.org>
2021-11-24 15:44:14 -0600

    Initial Commit

---
+++ Light/LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightApp.swift
+//  Light
+//
+//  Created by Central States SER on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
